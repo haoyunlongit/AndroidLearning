dd3142ecfc817c67ecb92c3bc049bfe8bed03f81
haoyunlong <haoyunlong@xiaomi.com>
2020-07-03 07:05:43 +0800

    动画提交

--- Animation/app/src/main/java/com/hyl/animation/MainActivity.java
+++ Animation/app/src/main/java/com/hyl/animation/MainActivity.java
@@ -4,16 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.handlerView) TextView animationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
-    public void clickMethod(View v) {
+    /**
+     * 学习animation 相关方法
+     * ===================================
+     */
+    public void animation1Method(View v) {
+        Animation animation = new TranslateAnimation(0, 200, 0, 200);
+        animation.setDuration(2000);
+        animation.setFillAfter(true);
+        animationView.startAnimation(animation);
+    }
+
+    public void animation2Method(View v) {
         System.out.println("~~~~~~~~~~~~~");
     }
+
+    public void animation3Method(View v) {
+        System.out.println("~~~~~~~~~~~~~");
+    }
+    /**
+     *===================================
+     */
 }
