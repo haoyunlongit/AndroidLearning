9c6e168593f2f36d4e51879ecf245e6a3f6be10d
haoyunlong <haoyunlong@xiaomi.com>
2020-11-08 21:52:21 +0800

    提交股票交易代码
    Signed-off-by: haoyunlong <haoyunlong@xiaomi.com>

---
+++ AlgrothmEasy/src/plan/MaxProfit.java
@@ -0,0 +1,35 @@
+package plan;
+
+public class MaxProfit {
+
+    public int maxProfit(int[] prices) {
+        int total = 0;
+        int buy = 0;
+        int sell = 0;
+
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (buy == 0 && prices[i + 1] > prices[i]) {
+                buy = prices[i];
+            }
+
+            if (sell == 0 && prices[i + 1] < prices[i]) {
+                sell = prices[i];
+            }
+
+            if (buy != 0 && sell != 0) {
+                total = (sell - buy);
+                sell = 0;
+                buy = 0;
+            }
+        }
+        if (buy != 0 && sell == 0) {
+            total += (prices[prices.length - 1] - buy);
+        }
+
+        return total;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
